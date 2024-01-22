ce3860c2dce33c47fab0bc4b6d13f321ff4ca682
petr.volkov <petr.volkov@megafon.ru>
2024-01-22 21:23:23 +0300

    edited styles, added zip buttons

--- board/__init__.py
+++ board/__init__.py
@@ -4,6 +4,8 @@ from board import pages
 
 def create_app():
     app = Flask(__name__)
+    app.jinja_env.globals.update(zip=zip)
     app.register_blueprint(pages.bp)
+    app.jinja_env.filters['zip'] = zip
     return app
  
--- board/pages.py
+++ board/pages.py
@@ -27,4 +27,7 @@ def dir_listing(req_path):
 
     # Show directory contents
     files = sorted(os.listdir(abs_path))
-    return render_template('pages/files.html', files=files)
+    files_is_dirs = [os.path.isdir(abs_path + file) for file in files]
+    print(files)
+    print(files_is_dirs)
+    return render_template('pages/files.html', files=files, files_is_dirs=files_is_dirs, zip=zip)
